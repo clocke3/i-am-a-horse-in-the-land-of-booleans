96d98fa583c7cd5896597b52843e0961782d1e79
Chihea Locke <clocke3@ycp.edu>
2017-10-10 20:20:50 -0400

    //done!

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,50 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn bool [x]
+  (if(or (= x nil) (= x false))
+  false
+  true))
 
-(defn abs [x]
-  ":(")
+(defn abs [x]  
+  (if (< x 0)
+    (- x) x))  
 
 (defn divides? [divisor n]
-  ":(")
+  (if ( = (mod n divisor) 0)
+    true
+    false))
+
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n)  "gotcha"
+    (divides? 5 n) "buzz"
+    (divides? 3 n) "fizz"
+    :else   ""))
 
 (defn teen? [age]
-  ":(")
+  (if(< 13 age 18)
+  true
+  false))
 
 (defn not-teen? [age]
-  ":(")
+  (cond
+  (or (< age 13) (> age 17)) true
+   :else false))
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond 
+    (number? x) (* 2 x)
+    (empty? x)  nil
+    (list? x) (* 2 (count x))
+    (vector? x) (* 2 (count x))
+    :else true))
 
 (defn leap-year? [year]
-  ":(")
+   (cond (=(mod year 400) 0) true
+        (=(mod year 100) 0) false
+        (= (mod year 4) 0) true
+        :else false))
 
 ; '_______'
